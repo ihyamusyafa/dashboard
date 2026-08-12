618433a12ea67f66c1812205ce0c8f6953fdbda3
Ihya Ahmad Musyafa <ihyaahmad0125@gmail.com>
2026-08-12 23:22:11 +0700

    update Supabase DB config

--- config/database.php
+++ config/database.php
@@ -1,25 +1,12 @@
 <?php
-
-return [
-
-    'default' => env('DB_CONNECTION', 'pgsql'),
-
-    'connections' => [
-
-         'pgsql' => [
-        'driver'   => 'pgsql',
-        'host'     => env('DB_HOST', 'aws-0-ap-southeast-2.pooler.supabase.com'),
-        'port'     => env('DB_PORT', '6543'), // port pooler Supabase
-        'database' => env('DB_DATABASE', 'postgres'),
-        'username' => env('DB_USERNAME', 'postgres.bfwyxlovlfiswcexdibt'), // sesuai user di dashboard
-        'password' => env('DB_PASSWORD', 'Magangbni2026'), // password Supabase kamu
-        'charset'  => 'utf8',
-        'prefix'   => '',
-        'schema'   => 'public',
-        'sslmode'  => 'require',
-        ],
-
-
-    ],
-
-];
+try {
+    $conn = new PDO(
+        "pgsql:host=aws-0-ap-southeast-2.pooler.supabase.com;port=6543;dbname=postgres;sslmode=require",
+        "postgres.bfwyxlovlfiswcexdibt", // username unik dari Supabase
+        "Magangbni2026"                  // password dari Supabase
+    );
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+?>
